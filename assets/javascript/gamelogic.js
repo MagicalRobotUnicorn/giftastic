773fe396b28a58616d4e85f3e1c5d28f103b5a7e
John Weber <johnhenryweber@gmail.com>
2019-08-23 22:45:20 -0700

    Landing Page Finished

--- assets/javascript/gamelogic.js
+++ assets/javascript/gamelogic.js
@@ -1,1 +1,17 @@
+// Javscript, CSS put it on monster
+// Reference -> Ask Tim for reference
+
+// Github, LinkedIn (Friend Tim), Monster
+
+// Read Evaluate Print Loop
+
+// Sorting hat function
+
+
+// 'Magic Wand' function
+
+
+// Create Button for search term
+
+
 
