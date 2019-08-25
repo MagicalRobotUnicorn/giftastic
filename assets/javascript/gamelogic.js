9540902a93d411a6082296a8590f3d25a42bb829
John Weber <johnhenryweber@gmail.com>
2019-08-24 22:09:24 -0700

    Finished Game Logic

--- assets/javascript/gamelogic.js
+++ assets/javascript/gamelogic.js
@@ -10,6 +10,9 @@
 // Potential waiting meme?
 // https://media.giphy.com/media/gMisXTaOeVw6Q/giphy.gif
 
+// Spiral Card Back
+// https://media.giphy.com/media/hoQKg8xPg35Sg/giphy.gif
+
 var houses = {
   "Hufflepuff": 'PMp40oEvNfKve',
   "Slytherin": 'A1xhQcogWZAIg',
@@ -48,8 +51,27 @@ function findGifs(searchTerm) {
   $.ajax({
     url: queryURL,
     method: 'GET'
-  }).then(function(respsonse) {
-    $('#exampleDiv').append("Success");
+  }).then(function(response) {
+    var total;
+    if (response.length < 10){
+      total = response.length;
+    }
+    else {
+      total = 10;
+    }
+
+    var rowTotal = 0;
+
+    var $newRow = $('<div class="row">')
+    for (var i = 0; i < total; i++){
+      var $newCol = $('<div class="row-md-3">');
+      var $newImage = $('<img src="' + response.data[i].images.fixed_height_still.url + '" class="gifStill data-gifAnimated="' + ">');
+      var $newRating = $('<p >' + response.data[i].rating + '<p>')
+      $newCol.append($newImage);
+      $newCol.append($newRating);
+
+
+    }
   });
 }
 
@@ -61,7 +83,9 @@ function createButton(value) {
 
   $('#buttonSection').append($newButton);
 }
-// Create Button for search term
 
+$('.gifStill').on('click', function() {
+
+})
 
 
