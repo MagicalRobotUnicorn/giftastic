8fdc4e61147e33a853ca74b45a05f2a661103ef8
John Weber <johnhenryweber@gmail.com>
2019-08-25 21:57:07 -0700

    Final Commit

--- assets/javascript/gamelogic.js
+++ assets/javascript/gamelogic.js
@@ -1,18 +1,6 @@
-// Javscript, CSS put it on monster
-// Reference -> Ask Tim for reference
+// Sorting Hat Functions
 
-// Github, LinkedIn (Friend Tim), Monster
-
-// Read Evaluate Print Loop
-
-// Sorting hat function
-
-// Potential waiting meme?
-// https://media.giphy.com/media/gMisXTaOeVw6Q/giphy.gif
-
-// Spiral Card Back
-// https://media.giphy.com/media/hoQKg8xPg35Sg/giphy.gif
-
+// Houses for sorting hat function (location on giphy)
 var houses = {
   "Hufflepuff": 'PMp40oEvNfKve',
   "Slytherin": 'A1xhQcogWZAIg',
@@ -20,6 +8,7 @@ var houses = {
   "Ravenclaw": '69xNNUtTkQK9q'
 }
 
+// Ajax call to Harry Potter API for finding the House
 function sortingHat(){
   var queryURL = 'https://www.potterapi.com/v1/sortingHat/?key=$2a$10$keI8PT9iiqnbBl/NUjDhZeDd9X1cVemSK.K5eOE7ZH0maFQH9dQ1K';
 
@@ -31,11 +20,7 @@ function sortingHat(){
   });
 }
 
-$('#clicktoSort').on("click", function() {
-  sortingHat();
-});
-
-
+// Function to change the cardback image to the corresponding house through the giphy api
 function displayHouse(response) {
   var queryURL = 'https://api.giphy.com/v1/gifs/' + houses[response] + '?api_key=T9x1MWUOUhdZlJPCkwFiuhWl4Rpcewza';
 
@@ -43,15 +28,27 @@ function displayHouse(response) {
     url: queryURL,
     method: "GET"
   }). then(function(response) {
-    // $('#houseGif').append(('<div id="resultGifDiv"><img src="' + response.data.images.original.url  + '" id="resultGif"></div>'));
     $('#houseImage').attr('src', response.data.images.original.url);
     flipCard();
   });
 }
 
-// 'Magic Wand' function
-// API Call: api.giphy.com/v1/gifs/search?api_key=T9x1MWUOUhdZlJPCkwFiuhWl4Rpcewza&q=hamburgers
+// Flip sorting hat function
+function flipCard() {
+  var userCard = document.getElementById('houseReveal');
+  userCard.classList.remove('flip-card-inner');
+  setTimeout(userCard.classList.add('flip-card-inner'), 2000);
+}
 
+// On Click function for sorting hat
+$('#clicktoSort').on("click", function() {
+  sortingHat();
+});
+
+
+// GIF Magic Functions
+
+// Function to find the relavant gifs on giphy
 function findGifs(searchTerm) {
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=T9x1MWUOUhdZlJPCkwFiuhWl4Rpcewza&q=" + searchTerm;
   
@@ -93,6 +90,7 @@ function findGifs(searchTerm) {
   });
 }
 
+// Create buttons from a search term
 function createButton(value) {
 
   var $newButtonDiv = $('<div class="col-md-2 spellButtonDivs">');
@@ -105,6 +103,7 @@ function createButton(value) {
   $('#preparedSpells').append($newButtonDiv);
 }
 
+// Create the initial buttons
 function initialButtons() {
   var autoButtons = ['magical', 'robot', 'unicorn', 'javascript', 'python', 'Node Js', 'Linux', 'Oregon'];
 
@@ -113,48 +112,28 @@ function initialButtons() {
   }
 }
 
+// Initialization function
 $(document).ready(function() {
   initialButtons();
 });
 
-$('.gifButton').on('click', function(){
-  $('#gifDisplay').html('');
-
-  var searchTerm = $(this).attr('id');
-
-  findGifs(searchTerm);
-})
-
+// Onclick function for animating gifs
 $('body').on('click', 'img.gifStill', function() {
   var temp = $(this).attr('data-gifAnimated');
   $(this).attr('data-gifAnimated', $(this).attr('src'));
   $(this).attr('src', temp);
 })
 
+// On click function to create gifs from each button
 $("body").on("click", "button.btn.btn-warning.gifButton", function () {
   var searchTerm = $(this).attr('id');
   findGifs(searchTerm);
 });
 
+// On click function for creating GIFs from buttons
 $("body").on("click", "button#prepareSpell.btn.btn-primary", function() {
   var newSpell = $('#spellInput').val();
 
   $('#spellInput').val('');
   createButton(newSpell);
-
 });
-
-
-// Flip sorting hat function
-function flipCard() {
-  var userCard = document.getElementById('houseReveal');
-  userCard.classList.remove('flip-card-inner');
-  setTimeout(userCard.classList.add('flip-card-inner'), 2000);
-}
-
-// TODO:
-// Import flipping script from RPS
-// Add Home buttons to both screens
-// Refactor Code to include two JS files, two css
-// Write comments
-// Write Readme
