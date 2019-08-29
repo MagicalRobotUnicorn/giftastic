0978464f3032145874990da7dc4ed76d8c825859
John Weber <johnhenryweber@gmail.com>
2019-08-29 07:18:37 -0700

    All functionality included, serious layout issues

---
+++ assets/javascript/favoriteslogic.js
@@ -0,0 +1,97 @@
+// We are using the same function
+// length will be the unpacked storedNames.length
+
+
+function displayFavorites() {
+  $('#gifDisplay').html('');
+
+  var storedNames = JSON.parse(localStorage.getItem("favoriteGifs"));
+
+  
+    var rowTotal = 0;
+
+    var $newRow = $('<div class="row">')
+    for (var i = 0; i < storedNames.length; i++){
+
+      var queryURL = 'https://api.giphy.com/v1/gifs/' + storedNames[i] + '?api_key=T9x1MWUOUhdZlJPCkwFiuhWl4Rpcewza';
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }). then(function(response) {
+        var $newCol = $('<div class="col-md-3 gifCard">');
+        var $newImage = $('<div class="gifImageDiv"><img src="' + response.data.images.fixed_height_still.url + '" class="gifStill" data-gifAnimated="' + response.data.images.fixed_height.url + '"></div>');
+        var $newDate = $('<div class="gifUploadDiv">Upload Date: ' + response.data.import_datetime + '</div>');
+        var $newRating = $('<div class="gifRatingDiv"><center><img src="./assets/images/filmratings/' + response.data.rating + '.png"></div>')
+        var $newButton = $('<button type="button" class="btn btn-outline-light unfavorite" id="' + response.data.id + '">Remove from Favorites</button>');
+        $newCol.append($newImage);
+        $newCol.append($newRating);
+        $newCol.append($newButton);
+        $newCol.append($newDate);
+        $newRow.append($newCol);
+        rowTotal++;
+      });
+
+      if (rowTotal > 2) {
+        $('#gifDisplay').append($newRow);
+        $newRow = $('<div class="row">')
+        rowTotal = 0;
+      }
+
+      if (i === (storedNames.length -1)){
+        $(gifDisplay).append($newRow);
+      }
+    }
+
+}
+
+function removeFavorite(id){
+
+  var storedNames = JSON.parse(localStorage.getItem("favoriteGifs"));
+
+  var totalItems = storedNames.length;
+
+  for (var i = 0; i < totalItems; i++){
+    if (storedNames[i] === id) {
+      for (var j = i; j < totalItems; j++) {
+        storedNames[j] = storedNames[j + 1];
+      }
+      totalItems -= 1;
+    }
+  }
+
+  var newArray = [];
+
+  for (var i= 0; i < totalItems; i++) {
+    newArray.push(storedNames[i]);
+  }
+
+  localStorage.setItem("favoriteGifs", JSON.stringify(newArray));
+  alert("Favorite Removed!");
+  displayFavorites();
+}
+
+// Onclick function for animating gifs
+$('body').on('click', 'img.gifStill', function() {
+  var temp = $(this).attr('data-gifAnimated');
+  $(this).attr('data-gifAnimated', $(this).attr('src'));
+  $(this).attr('src', temp);
+})
+
+$("body").on("click", "button.btn.btn-outline-light.unfavorite", function() {
+  var identifier = $(this).attr('id');
+  removeFavorite(identifier);
+})
+
+$(document).ready(function() {
+  displayFavorites();
+});
+
+// TO DO
+// disable button on sort
+// import moment.js and convert date time to readable date
+
+// add row below gif
+// float rating left, float button right
+// add default display on favorites page
+
--- assets/javascript/gamelogic.js
+++ assets/javascript/gamelogic.js
@@ -30,6 +30,7 @@ function displayHouse(response) {
   }). then(function(response) {
     $('#houseImage').attr('src', response.data.images.original.url);
     flipCard();
+    $('#sortingButtons').html('<button type="button" class="btn btn-primary" id="returnHome" onclick="location.href=\'./index.html\';">Return Home</button>');
   });
 }
 
@@ -50,6 +51,8 @@ $('#clicktoSort').on("click", function() {
 
 // Function to find the relavant gifs on giphy
 function findGifs(searchTerm) {
+  var storedNames = JSON.parse(localStorage.getItem("names"));
+
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=T9x1MWUOUhdZlJPCkwFiuhWl4Rpcewza&q=" + searchTerm;
   
   $('#gifDisplay').html('');
@@ -58,8 +61,8 @@ function findGifs(searchTerm) {
     method: 'GET'
   }).then(function(response) {
     var total;
-    if (response.length < 10){
-      total = response.length;
+    if (response.data.length < 10){
+      total = response.data.length;
     }
     else {
       total = 10;
@@ -71,9 +74,13 @@ function findGifs(searchTerm) {
     for (var i = 0; i < total; i++){
       var $newCol = $('<div class="col-md-3 gifCard">');
       var $newImage = $('<div class="gifImageDiv"><img src="' + response.data[i].images.fixed_height_still.url + '" class="gifStill" data-gifAnimated="' + response.data[i].images.fixed_height.url + '"></div>');
+      var $newDate = $('<div class="gifUploadDiv">Upload Date: ' + response.data[i].import_datetime + '</div>');
       var $newRating = $('<div class="gifRatingDiv"><center><img src="./assets/images/filmratings/' + response.data[i].rating + '.png"></div>')
+      var $newButton = $('<button type="button" class="btn btn-outline-light favorite" id="' + response.data[i].id + '">Favorite</button>');
       $newCol.append($newImage);
       $newCol.append($newRating);
+      $newCol.append($newButton);
+      $newCol.append($newDate);
       $newRow.append($newCol);
       rowTotal++;
 
@@ -87,8 +94,72 @@ function findGifs(searchTerm) {
         $(gifDisplay).append($newRow);
       }
     }
+    var $endRow = $('<div id="row">');
+
+    var $addingButton = $('<button type="button" class="btn btn-primary" id="addMoreGifs" data-searchTerm="' + searchTerm + '" data-iteration=1>Add 10 More Gifs</button>');
+    $endRow.append($addingButton);
+
+    $('#addingGifs').append($endRow);
   });
 }
+
+function addMoreGifs(searchTerm) {
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=T9x1MWUOUhdZlJPCkwFiuhWl4Rpcewza&q=" + searchTerm;
+  
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    var total;
+    if (response.data.length + 10 < 20){
+      total = response.data.length;
+    }
+    else {
+      total = 20;
+    }
+
+    var rowTotal = 0;
+
+    var $newRow = $('<div class="row">')
+    for (var i = 10; i < total; i++){
+      var $newCol = $('<div class="col-md-3 gifCard">');
+      var $newImage = $('<div class="gifImageDiv"><img src="' + response.data[i].images.fixed_height_still.url + '" class="gifStill" data-gifAnimated="' + response.data[i].images.fixed_height.url + '"></div>');
+      var $newDate = $('<div class="gifUploadDiv">Upload Date: ' + response.data[i].import_datetime + '</div>');
+      var $newRating = $('<div class="gifRatingDiv"><center><img src="./assets/images/filmratings/' + response.data[i].rating + '.png"></div>');
+      var $newButton = $('<button type="button" class="btn btn-outline-light favorite" id="' + response.data[i].id + '">Favorite</button>');
+      $newCol.append($newImage);
+      $newCol.append($newRating);
+      $newCol.append($newButton);
+      $newCol.append($newDate);
+      $newRow.append($newCol);
+      rowTotal++;
+
+      if (rowTotal > 2) {
+        $('#gifDisplay').append($newRow);
+        $newRow = $('<div class="row">')
+        rowTotal = 0;
+      }
+
+      if (i === (total -1)){
+        $(gifDisplay).append($newRow);
+      }
+    }
+
+    $('#addingGifs').html('');
+  });
+}
+
+// Flip sorting hat function
+function flipCard() {
+  var userCard = document.getElementById('houseReveal');
+  userCard.classList.remove('flip-card-inner');
+  setTimeout(userCard.classList.add('flip-card-inner'), 2000);
+}
+
+// On Click function for sorting hat
+$('#clicktoSort').on("click", function() {
+  sortingHat();
+});
 
 // Create buttons from a search term
 function createButton(value) {
@@ -130,6 +201,18 @@ $("body").on("click", "button.btn.btn-warning.gifButton", function () {
   findGifs(searchTerm);
 });
 
+// On click function to create 10 more gifs
+$("body").on("click", "button#addMoreGifs.btn.btn-primary", function () {
+  var searchTerm = $(this).attr('data-searchterm');
+  var iteration = $(this).attr('data-iteration');
+  addMoreGifs(searchTerm, iteration);
+});
+
+$("body").on("click", "button.btn.btn-outline-light.favorite", function() {
+  var identifier = $(this).attr('id');
+  addFavorite(identifier);
+})
+
 // On click function for creating GIFs from buttons
 $("body").on("click", "button#prepareSpell.btn.btn-primary", function() {
   var newSpell = $('#spellInput').val();
@@ -137,3 +220,67 @@ $("body").on("click", "button#prepareSpell.btn.btn-primary", function() {
   $('#spellInput').val('');
   createButton(newSpell);
 });
+
+function addFavorite(id){
+
+  var storedNames = JSON.parse(localStorage.getItem("favoriteGifs"));
+
+  if (storedNames === null) {
+    storedNames = [];
+  }
+  storedNames.push(id);
+
+  localStorage.setItem("favoriteGifs", JSON.stringify(storedNames));
+  alert("Favorite Saved!");
+  console.log(id);
+}
+
+
+
+
+// date added: use moment.js
+
+// Adding gifs to favorite section
+// button on top -> my favorites
+// button on each gif -> add to favorites
+
+// On clicking the add to favorites button:
+// The current names are unpacked
+// The name which was clicked is added to the collection
+// The collection is reserialized
+
+// local storage: the array
+// capture the gif id as a variable on the button
+// load that value into a function
+// rinse and repeat
+
+///////////////////////////////////////////
+
+// Diplay favorites page
+// De-serialize local storage
+// iterate through the loop
+// create a card for each gif id
+
+
+// function sortingHat(){
+//   var queryURL = 'https://www.potterapi.com/v1/sortingHat/?key=$2a$10$keI8PT9iiqnbBl/NUjDhZeDd9X1cVemSK.K5eOE7ZH0maFQH9dQ1K';
+
+//   $.ajax({
+//     url: queryURL,
+//     method: 'GET'
+//   }).then(function(response){
+//     displayHouse(response);
+//   });
+// }
+
+
+
+
+
+// var names = [];
+// names[0] = prompt("New member name?");
+// localStorage.setItem("names", JSON.stringify(names));
+
+// //...
+// var storedNames = JSON.parse(localStorage.getItem("names"));
+// using local storage
